58bc3f38f31035064c988b313aac767538c2eaab
ThisNameIsNotAvailable <aliaxeyka@gmail.com>
2023-05-19 22:44:45 +0200

    add networking including graphql and rest apis

--- CountryData/Models/CountryInfo.swift
+++ CountryData/Models/CountryInfo.swift
@@ -7,6 +7,57 @@
 
 import Foundation
 
+struct Name: Codable {
+    let official: String
+}
+
+struct CurrencyInfo: Codable {
+    let name: String
+    let shortName: String
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: CodingKeys.name)
+        shortName = container.codingPath.last!.stringValue
+    }
+}
+
+struct Currency: Codable {
+    let array: [CurrencyInfo]
+    
+    private struct DynamicCodingKeys: CodingKey {
+        var stringValue: String
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        var intValue: Int?
+        init?(intValue: Int) {
+            return nil
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DynamicCodingKeys.self)
+        
+        var tempArray = [CurrencyInfo]()
+        for key in container.allKeys {
+            let decodedObject = try container.decode(CurrencyInfo.self, forKey: DynamicCodingKeys(stringValue: key.stringValue)!)
+            tempArray.append(decodedObject)
+        }
+        array = tempArray
+    }
+}
+
 struct CountryInfo: Codable {
+    let name: Name
+    let capital: [String]?
+    let population: Int
+    let region: String
+    let subregion: String?
+    let currencies: Currency?
     
 }
--- CountryData/Models/CountryList.swift
+++ CountryData/Models/CountryList.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
+struct Country: Codable {
+    let name: String
+    let emoji: String
+}
+
+struct Countries: Codable {
+    let countries: [Country]
+}
+
+struct Continents: Codable {
+    let continents: [Countries]
+}
+
 struct CountryList: Codable {
-    
+    let data: Continents
 }
